705de59377b4ff6987cb587724dd4bf7ac22a11e
ShalviPachori72 <shalvipachori0@gmail.com>
2025-05-04 03:10:57 +0530

    Website completed !

---
+++ public/js/map.js
@@ -0,0 +1,11 @@
+
+let mapToken = process.env.MAP_TOKEN;
+
+console.log(mapToken);
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+    container : 'map', // container ID
+    center: [77.209, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    zoom: 9 // starting zoom
+});
